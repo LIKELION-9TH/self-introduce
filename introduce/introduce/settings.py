ad106c0c32632b5dbf10c5f1674fd538ef04e87e
hongin12 <ghddls0202@gmail.com>
2021-07-22 23:37:44 +0900

    base.html 만들어서 템플릿 상속(navbar와 footer), music.html 영상 삽입
    -footer에 문제가 있어 하단 고정 불가. 해결 필요

--- introduce/introduce/settings.py
+++ introduce/introduce/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'introduce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['introduce/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
